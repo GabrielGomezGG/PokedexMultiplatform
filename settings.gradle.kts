2c8f6636937c1727372c4e88cdd27681cb7207f6
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-16 19:01:34 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PokedexMultiplatform"
+include(":androidApp")
+include(":shared")
